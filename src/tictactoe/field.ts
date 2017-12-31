9c112ab2a2b2b9811153d39d148bd5cf8d4f7d1e
Isle87 <isle87@gmail.com>
2017-12-31 20:42:34 +0100

    Field input Logic

--- src/tictactoe/field.ts
+++ src/tictactoe/field.ts
@@ -1,27 +1,49 @@
 export class Field {
     public element: HTMLElement;
-    
+  
     private _index : number;
+    /** The index of the specific field */
     public get index() : number {
         return this._index;
     }
+
     
+    private _player : 'x' | 'o' | undefined = undefined;
+    public get player() : 'x' | 'o' | undefined {
+        return this._player;
+    }
+    public set player(v : 'x' | 'o' | undefined) {
+        console.log(this._player,v);
+        if(this._player) {
+            return;
+        }
+        this._player = v;
+        this.setStylePlayer();
+    }
+    
+    
+    /** The Color of the field */
     public color: string = 'gray';
+    /** The size of the Field */
     public size: number = 200;
-    public onClick = () => console.log('clicked ' + this.index);
+    public onClick: (index: number) => any;
 
     constructor(index: number) {
         this._index = index;
         this.element = document.createElement('div');
-        this.element.onclick = this.onClick;
-        this.styling();
+        this.element.onclick = (res) => {
+            this.onClick(this.index);
+        } 
+        this.stylingMain();
     }
 
-    private styling() {
-        this.element.style.cursor = 'pointer';
-        this.element.style.backgroundColor = this.color;
-        this.element.style.width = this.size + 'px';
-        this.element.style.height = this.size + 'px';
+
+    public reset() {
+        this._player = undefined;
+        for (let i = 0; i < this.element.children.length; i++) {
+            let ele = this.element.children[i];
+            ele.remove();      
+        }
     }
 
     public set innerHTML(v : string) {
@@ -31,6 +53,40 @@ export class Field {
     public set addClass(v : string) {
         this.element.classList.add(v);
     }
-    
+
+    private setStylePlayer() {
+        if(this.player === 'x')
+            this.styleCross();
+        else 
+            this.styleCircle();
+    }
+
+    /** Set the field to cross */
+    private styleCross() {
+        // for (let i = 0; i < 2; i++) {
+            const line =  document.createElement('div');
+            line.classList.add('cross');
+            //line.style.backgroundColor = 'red';
+            console.log('hello');
+            this.element.appendChild(line);
+       // }
+    }
+
+    /** Sets the field style to circle */
+    private styleCircle() {
+        const cir = document.createElement('div');
+        cir.classList.add('circle');
+        this.element.appendChild(cir);
+    }
+
+    private stylingMain() {
+        this.element.style.cursor = 'pointer';
+        this.element.style.backgroundColor = this.color;
+        this.element.style.width = this.size + 'px';
+        this.element.style.height = this.size + 'px';
+
+        this.element.onmouseenter = () => this.element.style.backgroundColor = 'lightgray';
+        this.element.onmouseleave = () => this.element.style.backgroundColor = this.color;
+    }  
     
 }
--- src/tictactoe/tictactoe.ts
+++ src/tictactoe/tictactoe.ts
@@ -2,10 +2,14 @@ import './style.css';
 import { Field } from './field'
 
 export class tictactoe {
+    private toggle = true;
+    private fields: Field[] = [];
+
     constructor(private element: HTMLElement) {
         //this.element.innerHTML= "Geil tic tac toe";
         this.setElementStyle();
         this.setUpfields();
+        this.setUpResetButton();
     }
 
     private setElementStyle() {
@@ -15,11 +19,29 @@ export class tictactoe {
     private setUpfields() {
         for (let i = 0; i < 9; i++) {
             const ele = new Field(i);
-            ele.innerHTML = i.toString();
             ele.addClass = 'box-' + i;
+            ele.onClick = (res) => this.setPlayer(res);
+            this.fields.push(ele);
             this.element.appendChild(ele.element);
         }
     }
 
+    setUpResetButton() {
+        const btn = document.createElement('button');
+        btn.onclick = () => {
+            for(let i= 0; i < this.fields.length; i++)
+                this.fields[i].reset();
+                // TODO: Logic reset
+        }
+        btn.textContent = 'Reset';
+        this.element.appendChild(btn);
+    }
+
+    private setPlayer(index:number) {
+        let field = this.fields.filter(ele => ele.index === index)[0];
+        console.log(field, this.toggle);
+        field.player = this.toggle? 'x': 'o';
+        this.toggle = !this.toggle;
+    }
 
 }
